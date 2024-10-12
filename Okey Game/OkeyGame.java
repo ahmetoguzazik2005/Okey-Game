f107d68cf3d5204f950f3d4ffb2335dc4f9cc1f6
Ahmet Oğuz Azık <ahmetoguzazik2005@gmail.com>
2024-10-12 18:10:04 +0300

    NEW 4

--- Okey Game/OkeyGame.java
+++ Okey Game/OkeyGame.java
@@ -161,7 +161,7 @@ public class OkeyGame {
         }
 
         // Returns true if there is no tile to pull
-        else if(tiles[index].equals(null) && players[getCurrentPlayerIndex()].getTiles()[14]==null){
+        else if(tiles[index].equals(null) ){
             System.out.println("Game is over ! Stalemate !");
             return true;
         }
